a86cba675c6339aef70fef2b38f95bea05fde6a6
DISNOTACAT <yeonjh915@gmail.com>
2024-06-26 10:45:47 +0900

    #61 메인과 로그인 페이지 정리 완료

---
+++ project/techmya/src/main/java/com/ohgiraffers/techmya/admin/order/OrderController.java
@@ -0,0 +1,4 @@
+package com.ohgiraffers.techmya.admin.order;
+
+public class OrderController {
+}
---
+++ project/techmya/src/main/java/com/ohgiraffers/techmya/controller/LoginController.java
@@ -0,0 +1,16 @@
+package com.ohgiraffers.techmya.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/login")
+@Controller
+public class LoginController {
+
+    @GetMapping("main")
+    public String getMain(){
+        return "login/main";
+    }
+
+}
